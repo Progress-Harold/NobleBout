2102b6c95c4b8602e162a49bde39ddaf76d20d5f
Anthony_torres44@icloud.com <anthony_torres44@icloud.com>
2020-09-14 13:50:40 -0700

    Added TitleScene

---
+++ NobleBout/NobleBout/SceneControllers/TitleSceneController.swift
@@ -0,0 +1,14 @@
+//
+//  TitleSceneController.swift
+//  NobleBout
+//
+//  Created by Anthony Torres on 9/14/20.
+//  Copyright © 2020 EightFoldGames. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class TitleSceneController: SKScene {
+    
+}
