4942c0df05cdacdcb76a8114d4bdfb8b3b5b1b80
Anthony_torres44@icloud.com <anthony_torres44@icloud.com>
2020-09-14 15:10:38 -0700

    Added particle emmiter and art for title

--- NobleBout/NobleBout/SceneControllers/TitleSceneController.swift
+++ NobleBout/NobleBout/SceneControllers/TitleSceneController.swift
@@ -11,4 +11,31 @@ import SpriteKit
 
 class TitleSceneController: SKScene {
     
+    var titleEmmiter: SKEmitterNode = SKEmitterNode()
+    
+    override func sceneDidLoad() {
+        super.sceneDidLoad()
+        
+        titleEmmiter = getParticles(name: "TitleFire")
+        
+        if let emmiter = self.childNode(withName: "TitleParticles") as? SKEmitterNode {
+            titleEmmiter.position = emmiter.position
+            self.addChild(titleEmmiter)
+        }
+    }
+    
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        print("User Tapped Screen")
+        //  TODO: - TODO add transtion to select screen https://noblesword.atlassian.net/browse/NB2-8
+        /*
+         Guard let scene = SelectSceneController(fileNamed: "SelectScene”)  else {
+                     return
+                 }
+                 
+                 scene.scaleMode = .aspectFill
+                 self.view?.presentScene(scene, transition: SKTransition.fade(withDuration: 0.4))
+         */
+    }
+    
 }
