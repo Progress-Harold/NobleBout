7b50467f7479120c0059d8cbb3a229167085774b
Anthony_torres44@icloud.com <anthony_torres44@icloud.com>
2020-09-28 15:36:44 -0700

    NB2-16-Character Selection functionality

--- NobleBout/NobleBout/SceneControllers/SelectScreenController.swift
+++ NobleBout/NobleBout/SceneControllers/SelectScreenController.swift
@@ -10,7 +10,45 @@ import SpriteKit
 
 class SelectScreenController: SKScene {
     
+    var charArray: [SKSpriteNode] = []
+    
+    // MARK: - Scene Life Cycle
+    
     override func sceneDidLoad() {
         super.sceneDidLoad()
+        
+        getCharButtons()
+    }
+    
+    
+    // MARK: - Setup Functions
+    
+    func getCharButtons() {
+        guard let camChildren = camera?.children else { return }
+        
+        for node in camChildren {
+            if rsCharStr.contains(node.name ?? "") {
+                if let cNode = node as? SKSpriteNode {
+                  charArray.append(cNode)
+                }
+            }
+        }
+    }
+    
+    
+    // MARK: - Handle Touches
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            for character in charArray {
+                if character.contains(location) {
+                    if let name = character.name {
+                        print("\(name)")
+                    }
+                }
+            }
+        }
     }
 }
