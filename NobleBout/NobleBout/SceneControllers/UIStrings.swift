7a10f332463188326c1fbe0a054285e9ce8c1d32
Anthony_torres44@icloud.com <anthony_torres44@icloud.com>
2020-10-02 18:27:41 -0700

    NB2-15-change placeholder icon selected

---
+++ NobleBout/NobleBout/SceneControllers/UIStrings.swift
@@ -0,0 +1,13 @@
+//
+//  UIStrings.swift
+//  NobleBout
+//
+//  Created by Anthony Torres on 10/2/20.
+//  Copyright © 2020 EightFoldGames. All rights reserved.
+//
+
+import Foundation
+
+// MARK: -
+
+let charPlaceholder: String = "charPlaceholder"
